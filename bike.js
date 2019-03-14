c3b2810b482d08f611af6ba713bb796be21328d4
Juan Portillo <juanportillog@gmail.com>
2019-03-14 16:26:04 +0100

    4players sin game over

--- bike.js
+++ bike.js
@@ -1,4 +1,5 @@
-function Bike (color, x, y, vx, vy, direction) {
+function Bike (color, x, y, vx, vy, direction, name) {
+  this.name = name;
   this.x = x;
   this.y = y;
   this.oldX = 0;
@@ -9,102 +10,113 @@ function Bike (color, x, y, vx, vy, direction) {
   this.currentDirection = direction;
   this.color = color;
   this.muerto = false;
-  // if (this.muerto) {
-  //   return;
-  // }
+
   this.update = function () {
     ctx = myGame.context;
     ctx.fillStyle = color;
-    if(this.muerto == false) {
+    if(this.muerto === false) {
       this.trail.push({x: this.x, y: this.y});
       this.trail.forEach(function(position) {
         ctx.fillRect(position.x, position.y, scale, scale);
-      })
+      }.bind(this))
     }
   };
+
   this.newPos = function() {
+    if(this.muerto === false) {
     this.x += this.speedX;
     this.y += this.speedY;
-  };
-  this.moveUp = function() { 
-    if (this.currentDirection !== "down") {
-    this.currentDirection = "up";
-    console.log(this.oldY, this.y);
-
-    if (this.oldX !== this.x) {
-    this.speedY = -1 * scale;
-    this.speedX = 0;
     };
-    this.oldX = this.x;
-    this.oldY = this.y;
-  };
-  };
-  this.moveDown = function() {
-    if (this.currentDirection !== "up") {
-    this.currentDirection = "down";
-    console.log(this.oldX, this.x);
-
-    if (this.oldX !== this.x) {
-    this.speedY = 1 * scale;
-    this.speedX = 0;
-    };
-    this.oldX = this.x;
-    this.oldY = this.y;
-  };
-  };
-  this.moveLeft = function() {
-    if (this.currentDirection !== "right") {
-    this.currentDirection = "left";
-    if (this.oldY !== this.y) {
-    this.speedY = 0;
-    this.speedX = -1 * scale;
-    };
-    this.oldX = this.x;
-    this.oldY = this.y;
-  };
-  };
-  this.moveRight = function() {
-    if (this.currentDirection !== "left") {
-    this.currentDirection = "right";
-    if (this.oldY !== this.y) {
-    this.speedY = 0;
-    this.speedX = 1 * scale;
-    };
-    this.oldX = this.x;
-    this.oldY = this.y;
-  };
   };
 };
 
+
+
 //colisión con trail
 Bike.prototype.collision = function(trail) {
-    for (var i=0; i<trail.length; i++) {
-      if (this.x === trail[i].x && this.y === trail[i].y) {
-        return true;
-      };
+  for (var i=0; i<trail.length; i++) {
+    if (this.x === trail[i].x && this.y === trail[i].y) {
+      return true;
     };
-    return false;
   };
+  return false;
+};
 
 //colisión con bordes
 Bike.prototype.bordes = function() {
-  return (this.x < 0 || this.x > myGame.canvas.width || this.y < 0 || this.y > myGame.canvas.height) 
+return (this.x < 0 || this.x > myGame.canvas.width || this.y < 0 || this.y > myGame.canvas.height) 
 }
 
+//muerte
 Bike.prototype.muerte = function() {
   // for (var i=0; i<this.trail.length; i++) {
   //   ctx.clearRect(this.trail[i].x, this.trail[i].y, scale, scale);
   // }
   this.muerto = true;
-  this.speedX=0;
-  this.speedY=0;
+  this.x=undefined;
+  this.y=undefined;
+  // this.speedX=0;
+  // this.speedY=0;
   this.trail = [];
 }
 
-var player1 = new Bike("blue", 0, 0, 1, 0, "right");
-var player2 = new Bike("red", myGame.canvas.width - scale, myGame.canvas.height - scale, -1, 0, "left");
-var player3 = new Bike("green", 0, myGame.canvas.height - scale, 0, -1, "up");
+//movimientos
+Bike.prototype.moveUp = function() { 
+  if (this.currentDirection !== "down") {
+  this.currentDirection = "up";
+  if (this.oldX !== this.x) {
+  this.speedY = -1 * scale;
+  this.speedX = 0;
+  };
+  this.oldX = this.x;
+  this.oldY = this.y;
+};
+};
+
+Bike.prototype.moveDown = function() {
+  if (this.currentDirection !== "up") {
+  this.currentDirection = "down";
+  if (this.oldX !== this.x) {
+  this.speedY = 1 * scale;
+  this.speedX = 0;
+  };
+  this.oldX = this.x;
+  this.oldY = this.y;
+};
+};
+
+Bike.prototype.moveLeft = function() {
+  if (this.currentDirection !== "right") {
+  this.currentDirection = "left";
+  if (this.oldY !== this.y) {
+  this.speedY = 0;
+  this.speedX = -1 * scale;
+  };
+  this.oldX = this.x;
+  this.oldY = this.y;
+};
+};
+
+Bike.prototype.moveRight = function() {
+  if (this.currentDirection !== "left") {
+  this.currentDirection = "right";
+  if (this.oldY !== this.y) {
+  this.speedY = 0;
+  this.speedX = 1 * scale;
+  };
+  this.oldX = this.x;
+  this.oldY = this.y;
+};
+};
+
+
+
+
+
 var player4 = new Bike("yellow", myGame.canvas.width - scale, 0, 0, 1, "down");
+var player3 = new Bike("green", 0, myGame.canvas.height - scale, 0, -1, "up");
+var player2 = new Bike("red", myGame.canvas.width - scale, myGame.canvas.height - scale, -1, 0, "left");
+var player1 = new Bike("blue", 0, 0, 1, 0, "right");
 
 
 
--- game.js
+++ game.js
@@ -11,6 +11,7 @@
   var pausado = false;
   var counter = 3;
   var players = 4;
+  var name = "";
 
   var myGame = {
     canvas: document.createElement("canvas"),
@@ -24,9 +25,15 @@
       document.getElementById('start-game').appendChild(this.canvas);
       this.interval = setInterval(updateMyGame, 90);
     },
+    pausa : function() {
+      this.context.font = "50px roboto";
+      this.context.fillStyle = "white";
+      this.context.textAlign = "center";
+      this.context.fillText("Pause", this.canvas.width/2, this.canvas.height/2);
+    },
     stop : function(winner) {
       clearInterval(this.interval);
-      this.context.font = "50px roboto";
+      this.context.font = "50px serif";
       this.context.fillStyle = winner;
       this.context.textAlign = "center";
       var pole = winner + " wins!";
@@ -38,11 +45,14 @@
   function updateMyGame () {
  
     if (pausado) { //PAUSA
+      myGame.pausa();
       return;
     }
-    myGame.context.clearRect(0, 0, myGame.canvas.width, myGame.canvas.height);
+    // myGame.context.clearRect(0, 0, myGame.canvas.width, myGame.canvas.height);
 
     if (players === 2) { //2 JUGADORES
+      myGame.context.clearRect(0, 0, myGame.canvas.width, myGame.canvas.height);
+
       player1.update();
       player2.update();
       player1.newPos();
@@ -51,30 +61,42 @@
       if (player1.collision(player2.trail) ||
        player1.collision(player1.trail) ||  player1.bordes()) {
         player1.muerte();
+
       } else if (player2.collision(player1.trail) || 
        player2.collision(player2.trail) || player2.bordes()) {
         player2.muerte();
+      };
 
-      } 
     } else if (players === 3) { //3 JUGADORES
+      myGame.context.clearRect(0, 0, myGame.canvas.width, myGame.canvas.height);
+
       player1.update();
       player2.update();
       player3.update();
       player1.newPos();
       player2.newPos();
       player3.newPos();
-      
+      // console.log(player1.y, player1.x);
+
       if (player1.collision(player1.trail) || player1.collision(player2.trail) || 
       player1.collision(player3.trail) || player1.bordes()) {
          player1.muerte();
+         console.log(player1.color, player1.muerto);
+
       } else if (player2.collision(player1.trail) || player2.collision(player2.trail) || 
       player2.collision(player3.trail) || player2.bordes()) {
          player2.muerte();
+         console.log(player2.color, player2.muerto);
+
       } else if (player3.collision(player1.trail) || player3.collision(player2.trail) || 
       player3.collision(player3.trail) || player3.bordes()) {
          player3.muerte();
-      } 
+         console.log(player3.color, player3.muerto);
+      }; 
+
     } else if (players === 4) { //4 JUGADORES
+      myGame.context.clearRect(0, 0, myGame.canvas.width, myGame.canvas.height);
+
       player1.update();
       player2.update();
       player3.update();
@@ -87,21 +109,22 @@
        if (player1.collision(player1.trail) || player1.collision(player2.trail) || 
        player1.collision(player3.trail) || player1.collision(player4.trail) || player1.bordes()) {
         player1.muerte();
+        console.log(player1.color, player1.muerto);
 
       } else if (player2.collision(player1.trail) || player2.collision(player2.trail) || 
       player2.collision(player3.trail) || player2.collision(player4.trail) || player2.bordes()) {
         player2.muerte();
+        console.log(player2.color, player2.muerto);
 
       } else if (player3.collision(player1.trail) || player3.collision(player2.trail) || 
       player3.collision(player3.trail) || player3.collision(player4.trail) || player3.bordes()) {
-        console.log(player3.color);
         player3.muerte();
+        console.log(player3.color, player3.muerto);
 
       } else if (player4.collision(player1.trail) || player4.collision(player2.trail) || 
       player4.collision(player3.trail) || player4.collision(player4.trail) || player4.bordes()) {
-        console.log(player4.color);
         player4.muerte();
-
+        console.log(player4.color, player4.muerto);
       };
     };
   };
